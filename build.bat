0d190500928dcacf68f86fb46be6dc312595d0ba
Ben Powell <benmorganpowell@hotmail.co.uk>
2015-10-12 17:44:22 +0200

    Tweaky tweaky tweaky

--- build.bat
+++ build.bat
@@ -14,11 +14,11 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\CouponCodeBuilder.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild CouponCodeBuilder.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
 mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 mkdir Build\lib\net45
 
-%nuget% pack "src\CouponCodeBuilder.1.0" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "CouponCodeBuilder.1.0" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
