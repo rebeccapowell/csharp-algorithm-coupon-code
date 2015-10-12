442589ea01374e80a361f46fdf107f50cdde88f3
Ben Powell <benmorganpowell@hotmail.co.uk>
2015-10-12 17:11:58 +0200

    Added build batch file.

---
+++ build.bat
@@ -0,0 +1,24 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild src\CouponCodeBuilder.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+
+mkdir Build
+mkdir Build\lib
+mkdir Build\lib\net40
+mkdir Build\lib\net45
+
+%nuget% pack "src\CouponCode.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
