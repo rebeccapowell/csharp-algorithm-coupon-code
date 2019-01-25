6291f9a1a089ad24416f61b0515926f20a0fdac1
Becca Powell <rebecca.powell@edmgroup.com>
2019-01-25 08:11:05 +0100

    Coupon Code Builder multi-platform

---
+++ Powells.CouponCode/Options.cs
@@ -0,0 +1,16 @@
+﻿namespace Powells.CouponCode
+{
+    public class Options
+    {
+        public int Parts { get; set; }
+        public int PartLength { get; set; }
+        public string Plaintext { get; set; }
+
+        public Options()
+        {
+            this.Parts = 3;
+            this.PartLength = 4;
+            this.Plaintext = "test";
+        }
+    }
+}
